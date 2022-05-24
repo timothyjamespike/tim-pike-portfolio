0a144ecac1e35693264d003a91992303cac33505
timothyjamespike <47398558+timothyjamespike@users.noreply.github.com>
2022-05-23 17:08:12 -0700

    Create BalancingFeedbackProject.js

---
+++ src/components/BalancingFeedbackProject.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const BalancingFeedbackProject = () => {
+    return (
+        <div className="content-filtering-container">
+            
+            <div className="content-filtering-description-container description-1">
+            <span className="content-filtering-description-subheader">Making content flexible</span>
+            <div clasName="customer-problem"></div>
+            <div className="content-filtering-description-container description-2">
+            <span className="customer-problem-eyebrow">Customer Problem</span>
+             "I'm just not ready to move to cloud. There are so many areas where I'm getting stuck!"
+            </div>
+            <div className="content-filtering-description-container description-3">
+            With the help of a service designer, our team consolidated <span className="strong-tags">user journeys</span> from disparate teams into an end-to-end blueprint.
+            </div>
+            <div className="content-filtering-description-container description-4">
+            This blueprint helped define the content guidance strategy for customers migrating from on-premises to cloud products.
+            <div className="content-filtering-image-1"></div>
+            <div className="content-filtering-image-2"></div>
+            </div>
+            </div>
+            <div className="content-filtering-images-container">
+            <div className="content-filtering-image-3">
+
+            </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default BalancingFeedbackProject;
